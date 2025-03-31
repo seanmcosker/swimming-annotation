7a566034b482637c28bad90d5713f399388e36c0
Sean <92123184+seanmcosker@users.noreply.github.com>
2025-03-31 21:51:05 +0000

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from botocore.exceptions import NoCredentialsError
 
 # AWS S3 Configuration from secrets.toml
 AWS_ACCESS_KEY = st.secrets["aws"]["AWS_ACCESS_KEY"]
-AWS_SECRET_KEY = st.secrets["AWS_SECRET_KEY"]
+AWS_SECRET_KEY = st.secrets['aws']["AWS_SECRET_KEY"]
 NON_ANNOTATED_BUCKET_NAME = st.secrets["NON_ANNOTATED_BUCKET_NAME"]
 ANNOTATED_BUCKET_NAME = st.secrets["ANNOTATED_BUCKET_NAME"]
 REGION_NAME = st.secrets["REGION_NAME"]
