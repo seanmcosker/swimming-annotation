cc4fc2bfe0f25683dcbb62d20e6e0da78027419f
seanmcosker <6mcoskers@gmail.com>
2025-04-15 22:34:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,20 +75,9 @@ elif page == "Video Viewer":
     videos = list_videos()
     if not videos:
         st.write("No videos found in the S3 bucket.")
-    else:
-        col1, col2 = st.columns(2)  # Create two columns for side-by-side display
-
-        with col1:
-            selected_video_1 = st.selectbox("Choose the first video to view:", videos, key="video1")
-            video_url_1 = get_video_url(selected_video_1)
-            st.video(video_url_1, format="video/mp4")
-            #st.write(f"Video 1 Source: {video_url_1}")
-
-        with col2:
-            selected_video_2 = st.selectbox("Choose the second video to view:", videos, key="video2")
-            video_url_2 = get_video_url(selected_video_2)
-            st.video(video_url_2, format="video/mp4")
-            #st.write(f"Video 2 Source: {video_url_2}")
+    selected_video_1 = st.selectbox("Choose the first video to view:", videos, key="video1")
+    video_url_1 = get_video_url(selected_video_1)
+    st.video(video_url_1, format="video/mp4")
 
 elif page == "Author Bio":
     st.title("Author Bio")
